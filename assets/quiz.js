67c2a8e7f0fc4b32b0824c382f6dd1639c477072
maris <marisandb@yahoo.com>
2021-06-24 20:46:12 -0500

    added foundational html  css and js

---
+++ assets/quiz.js
@@ -0,0 +1,13 @@
+var score = 0;
+
+var startButton = document.querySelector("#start");
+
+//array of quiz questions
+var questions = [
+    {q: 'Inside which HTML element do we put the JavaScript?'},
+    {q: 'How can you add a comment in a JavaScript?'},
+    {q: 'What is the correct way to write a JavaScript array?'},
+    {q: 'How do you find the number with the highest value of x and y?'},
+    {q: 'JavaScript is the same as Java.'},
+];
+
