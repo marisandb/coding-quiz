118890dc05c32e8b7ea026e1a5de3997ca309633
maris <marisandb@yahoo.com>
2021-06-26 01:22:15 -0500

    added timer function, display question function (broken), and start function

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,54 +1,121 @@
-var score = 0;
+let score = 0;
+timer = 60;
 
-var startButton = document.querySelector("#start");
-var quizContainer = document.querySelector("#quiz");
+const startButton = document.querySelector("#start");
+const quizEl = document.querySelector("#quiz-container");
+const timerEl = document.querySelector("#timer");
 
 //array of quiz questions with answers
-var questions = [
+let myQuestions = [
     {q: 'Inside which HTML element do we put the JavaScript?',
-        answers: {
-            a: "<script>",
-            b: "<javascript>",
-            c: "<scripting>",
-            d: "<js>"
-        },
-        correctAnswer: "a"
+        answers: [
+        "<script>",
+        "<javascript>",
+        "<scripting>",
+        "<js>"
+        ],
+        correctAnswer: "<script>"
     },
     {q: 'How can you add a comment in a JavaScript?',
-        answers: {
-            a: "'This is a comment",
-            b: " //This is a comment",
-            c: "<!--This is a comment-->",
-            d: "-This is a comment-"   
-        },
-        correctAnswer: "b"
+        answers: [
+            "'This is a comment",
+            " //This is a comment",
+            "<!--This is a comment-->",
+            "-This is a comment-"   
+        ],
+        correctAnswer: "//This is a comment"
     },
     {q: 'What is the correct way to write a JavaScript array?', 
-        answers: {
-            a:`var colors = (1:"red",2:"green",3:"blue")`,
-            b:`var colors = "red","green","blue"`,
-            c:`var colors = 1=("red"), 2=("green"), 3=("blue")`,
-            d:`var colors = ["red","green","blue"]`
-        },
-        correctAnswer: "d"
+        answers: [
+            `var colors = (1:"red",2:"green",3:"blue")`,
+            `var colors = "red","green","blue"`,
+            `var colors = 1=("red"), 2=("green"), 3=("blue")`,
+            `var colors = ["red","green","blue"]`
+        ],
+        correctAnswer: `var colors = ["red","green","blue"]`
     },
     {q: 'How do you find the number with the highest value of x and y?',
-        answers: {
-            a: "Math.max(x, y)",  
-            b: "ceil(x, y)",
-            c: "Math.ceil(x, y)",
-            d: "top(x, y)"
-        },
-        correctAnswer: "a"
+        answers: [
+            "Math.max(x, y)",  
+            "ceil(x, y)",
+            "Math.ceil(x, y)",
+            "top(x, y)"
+        ],
+        correctAnswer: "Math.max(x, y)"
     },
     {q: 'Which event occurs when the user clicks on an HTML element?',
-        answers: {
-            a:"onmouseclick",
-            b:"onmouseover",
-            c:"onclick",
-            d:"onchange"
-        },
-        correctAnswer: "c"
+        answers: [
+            "onmouseclick",
+            "onmouseover",
+            "onclick",
+            "onchange"
+        ],
+        correctAnswer: "onclick"
     }
 ];
 
+//start quiz function
+function startQuiz() {
+    //this will remove the intial laod page when the user clicks start
+    let quizStart = document.getElementById("quiz-start");
+    quizStart.remove();
+
+    displayQuestions();
+    countdown();
+}
+
+//this function will display the question/answer choices
+function displayQuestions() {
+    let firstQ = 0;
+    //create div to display the question 
+    var questionDisplayEl = document.createElement("div");
+    questionDisplayEl.textContent = myQuestions[firstQ].q;
+    questionDisplayEl.className = "question-display";
+    quizEl.appendChild(questionDisplayEl);
+    
+    //adds answer choices to the question in button form
+    let displayQ = function () {
+    for (var i = 0; i < myQuestions[firstQ].answers.length; i++){
+    let answerChoices = document.createElement("button");
+    answerChoices.textContent = myQuestions.answers;
+    answerChoices.className = "btn answer-choices";
+    questionDisplayEl.appendChild(answerChoices);
+    }
+    };
+    displayQ();
+    
+};
+startButton.addEventListener("click", startQuiz);
+//check questions for correct answer
+
+
+//for(var i=0; i < myQuestions.length; i++){
+
+   // var answer = confirm(myQuestions[i].correctAnswer);
+
+ //   if( ??? )
+    
+    //increases score
+   // score++;
+//} 
+
+//timer function
+function countdown() {
+    let timeLeft = 60;
+
+    const timeInterval = setInterval(function() {
+        if (timeLeft >= 0) {
+            timerEl.textContent = timeLeft;
+            timeLeft--;
+        }
+        else {
+            setTimeout(timeInterval,1000);
+        }
+    }, 1000);
+}
+
+
+//countdown();
+//startButton.onclick = countdown;
+
+//high score function
